7247f50c280d5f1c01878288f3a145b552b76412
free6nk33 <free6nk33@gmail.com>
2024-12-03 18:14:50 +0100

    Update Options Scanner

--- scanner.cpp
+++ scanner.cpp
@@ -3,7 +3,7 @@
 #include <regex>
 #include <vector>
 #include <thread>
-#include <cstring> 
+#include <cstring>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
@@ -11,7 +11,7 @@
 
 using namespace std;
 
-
+// Fonction pour récupérer la version
 string getVersion(const string& ip, int port) {
     int sock;
     struct sockaddr_in server;
@@ -19,7 +19,7 @@ string getVersion(const string& ip, int port) {
 
     sock = socket(AF_INET, SOCK_STREAM, 0);
     if (sock == -1) {
-        cerr << "Erreur de creation du socket" << endl;
+        cerr << "Erreur de création du socket" << endl;
         return "Erreur";
     }
 
@@ -27,18 +27,17 @@ string getVersion(const string& ip, int port) {
     server.sin_port = htons(port);
     inet_pton(AF_INET, ip.c_str(), &server.sin_addr);
 
-
     if (connect(sock, (struct sockaddr*)&server, sizeof(server)) < 0) {
         cerr << "Impossible de se connecter à " << ip << ":" << port << endl;
         close(sock);
         return "Erreur";
     }
 
-    //Reception de la version
+    // Réception de la version
     memset(buffer, 0, sizeof(buffer));
     ssize_t bytesReceived = recv(sock, buffer, sizeof(buffer) - 1, 0);
     if (bytesReceived < 0) {
-        cerr << "Erreur de recuperation de la version" << endl;
+        cerr << "Erreur de récupération de la version" << endl;
         close(sock);
         return "Erreur";
     }
@@ -57,43 +56,74 @@ void scanPort(const string& ip, int port) {
 
     if (regex_search(banner, matches, versionRegex)) {
         switch (port) {
+            case 80:
+                cout << " (HTTP/TCP)    version " << matches[0];
+                break;
+            case 443:
+                cout << " (HTTPS/TCP)   version " << matches[0];
+                break;
+            case 22:
+                cout << " (SSH/TCP)     version " << matches[0];
+                break;
+            case 21:
+                cout << " (FTP/TCP)     version " << matches[0];
+                break;
             case 25:
-                cout << " (SMTP)   version " << matches[0];
+                cout << " (SMTP/TCP)    version " << matches[0];
                 break;
             case 110:
-                cout << " (POP3)  version " << matches[0];
+                cout << " (POP3/TCP)    version " << matches[0];
                 break;
             case 3306:
-                cout << " (MySQL)  version " << matches[0];
+                cout << " (MySQL/TCP)   version " << matches[0];
                 break;
             case 23:
-                cout << " (Telnet) version " << matches[0];
+                cout << " (Telnet/TCP)  version " << matches[0];
                 break;
             case 53:
-                cout << " (DNS)    " << matches[0];
+                cout << " (DNS/TCP)     version " << matches[0];
                 break;
             default:
-                cout << " (Service inconnu) version " << matches[0];
+                cout << " (Service inconnu/?) version " << matches[0];
+                break;
         }
-        cout << endl;
     } else {
-        cout << ": Version non trouvée" << endl;
+        cout << ": Version non trouvée ou bannière vide";
     }
+    cout << endl;
 }
 
 int main() {
     string ip;
-    vector<int> ports = {22, 25, 53, 80, 110, 443, 3306, 23}; //Les ports a scan
+    vector<int> ports = {22, 25, 53, 80, 110, 443, 3306, 23}; // Les ports à scanner
+    int choix;
 
-    cout << "IP : ";
-    cin >> ip;
+    cout << "1) Scanner tous les ports" << endl;
+    cout << "2) Scanner uniquement HTTP/HTTPS et SSH" << endl;
+    cout << "NSC> ";
+    cin >> choix;
 
     vector<thread> threads;
 
-    for (int port : ports) {
-        threads.emplace_back(scanPort, ip, port);
+    if (choix == 1) {
+        cout << "IP : ";
+        cin >> ip;
+        for (int port : ports) {
+            threads.emplace_back(scanPort, ip, port);
+        }
+    } else if (choix == 2) {
+        cout << "IP : ";
+        cin >> ip;
+        vector<int> port_ssh_http = {22, 80, 443};
+        for (int port : port_ssh_http) {
+            threads.emplace_back(scanPort, ip, port);
+        }
+    } else {
+        cout << "Choix invalide !" << endl;
+        return 1;
     }
 
+    // Attente de la fin des threads
     for (auto& t : threads) {
         t.join();
     }
