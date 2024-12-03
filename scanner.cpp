146ea005205d820aa636237d74d0e6a212e7ff79
free6nk33 <free6nk33@gmail.com>
2024-12-03 17:58:27 +0100

    Update + Résolution des problèmes

--- scanner.cpp
+++ scanner.cpp
@@ -1,138 +1,102 @@
 #include <iostream>
 #include <string>
+#include <regex>
 #include <vector>
-#include <cstring>
-#include <netdb.h>
-#include <unistd.h>
-#include <arpa/inet.h>
 #include <thread>
-#include <mutex>
+#include <cstring> 
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
 
 using namespace std;
 
-mutex outputMutex; // Pour synchroniser les sorties des threads
-string getBanner(const string &ip,int port) {
+
+string getVersion(const string& ip, int port) {
     int sock;
     struct sockaddr_in server;
     char buffer[1024];
 
-    sock = socket(AF_INET,SOCK_STREAM,0);
-    if (sock < 0){
-        cerr << "Erreur creation du socket";
-        return "";
-    }
-    memset(&server,0,sizeof(server))
-    server.sin_family = AF_INET; 
-    server.sin_port = htons(port);
-    inet_pton(AF_INET, ip.c_str(), &server.sin_addr);
-
-    if(connect(sock,(struct sockaddr *)&server, sizeof(server)) == 0){
-        recv(sock,buffer,sizeof(buffer) - 1.0);
-    }else{
-        close(sock);
-        return "";
-    }
-    close(sock);
-    return string(buffer);
-}
-
-void scanPort(const string &ip, int port, const string &banner) {
-
-    regex versionRegex(R"(([0-9]+\.[0-9]+(\.[0-9]+)?))");
-    smatch matches;
-    if(regex_search(banner,matches,versionRegex)) {
-        return matches[0];
-    }else{
-        return "Version non trouvee";
-    }
-
-
-    int sock;
-    struct sockaddr_in server;
-
     sock = socket(AF_INET, SOCK_STREAM, 0);
-    if (sock < 0) {
-        cerr << "Erreur creation du socket\n";
-        return;
+    if (sock == -1) {
+        cerr << "Erreur de creation du socket" << endl;
+        return "Erreur";
     }
 
-    memset(&server, 0, sizeof(server));
     server.sin_family = AF_INET;
     server.sin_port = htons(port);
     inet_pton(AF_INET, ip.c_str(), &server.sin_addr);
 
-    // Tentative de connexion
-    if (connect(sock, (struct sockaddr *)&server, sizeof(server)) == 0) {
-        lock_guard<mutex> lock(outputMutex); // Empêche les sorties concurrentes
-        cout << "Port " << port << " est ouvert";
 
-        // Détection des services courants
-        switch (port) {
-            case 22: cout << " (SSH)    version" << matches[0]; break;
-            case 21: cout << " (FTP)    version" << matches[0]; break;
-            case 80: cout << " (HTTP)   version" << matches[0]; break;
-            case 443: cout << " (HTTPS) version" << matches[0]; break;
-            case 25: cout << " (SMTP)   version" << matches[0]; break;
-            case 110: cout << " (POP3)  version" << matches[0]; break;
-            case 3306: cout << " (MySQL)    version" << matches[0]; break;
-            case 23: cout << " (Telnet) version" << matches[0]; break;
-            case 53: cout << " (DNS)    " << matches[0]; break;
-            default: cout << " (Service inconnu)";
-        }
-        cout << endl;
+    if (connect(sock, (struct sockaddr*)&server, sizeof(server)) < 0) {
+        cerr << "Impossible de se connecter à " << ip << ":" << port << endl;
+        close(sock);
+        return "Erreur";
+    }
+
+    //Reception de la version
+    memset(buffer, 0, sizeof(buffer));
+    ssize_t bytesReceived = recv(sock, buffer, sizeof(buffer) - 1, 0);
+    if (bytesReceived < 0) {
+        cerr << "Erreur de recuperation de la version" << endl;
+        close(sock);
+        return "Erreur";
     }
 
     close(sock);
+    return string(buffer);
 }
 
-void scanRange(const string &ip, int startPort, int endPort, int numThreads) {
-    vector<thread> threads;
+// Fonction pour scanner un port
+void scanPort(const string& ip, int port) {
+    string banner = getVersion(ip, port);
+    regex versionRegex(R"(([0-9]+\.[0-9]+(\.[0-9]+)?))");
+    smatch matches;
 
-    for (int port = startPort; port <= endPort; ++port) {
-        threads.emplace_back(scanPort, ip, port);
+    cout << "Port " << port;
 
-        if (threads.size() >= numThreads) {
-            for (auto &t : threads) {
-                if (t.joinable()) t.join();
-            }
-            threads.clear();
+    if (regex_search(banner, matches, versionRegex)) {
+        switch (port) {
+            case 25:
+                cout << " (SMTP)   version " << matches[0];
+                break;
+            case 110:
+                cout << " (POP3)  version " << matches[0];
+                break;
+            case 3306:
+                cout << " (MySQL)  version " << matches[0];
+                break;
+            case 23:
+                cout << " (Telnet) version " << matches[0];
+                break;
+            case 53:
+                cout << " (DNS)    " << matches[0];
+                break;
+            default:
+                cout << " (Service inconnu) version " << matches[0];
         }
-    }
-
-    for (auto &t : threads) {
-        if (t.joinable()) t.join();
+        cout << endl;
+    } else {
+        cout << ": Version non trouvée" << endl;
     }
 }
 
 int main() {
-    string targetIP;
-    int startPort = 1, endPort = 65535;
-    int numThreads = 10; 
-    int choix;
+    string ip;
+    vector<int> ports = {22, 25, 53, 80, 110, 443, 3306, 23}; //Les ports a scan
 
-    cout << "1) Scanner tous les ports (1-65535)" << endl;
-    cout << "2) Scanner une plage de ports spécifique" << endl;
-    cout << "Entrez votre choix : ";
-    cin >> choix;
+    cout << "IP : ";
+    cin >> ip;
 
-    if (choix == 1) {
-        cout << "Entrez l'adresse IP cible : ";
-        cin >> targetIP;
-        cout << "Scan de l'adresse " << targetIP << " sur les ports de " << startPort << " a " << endPort << "...\n";
-        scanRange(targetIP, startPort, endPort, numThreads);
-    } else if (choix == 2) {
-        cout << "Entrez l'IP cible : ";
-        cin >> targetIP;
-        cout << "Entrez le port de depart : ";
-        cin >> startPort;
-        cout << "Entrez le port de fin : ";
-        cin >> endPort;
-        cout << "Scan de l'adresse " << targetIP << " sur les ports de " << startPort << " a " << endPort << "...\n";
-        scanRange(targetIP, startPort, endPort, numThreads);
-    } else {
-        cout << "Choix invalide. Fin du programme.\n";
+    vector<thread> threads;
+
+    for (int port : ports) {
+        threads.emplace_back(scanPort, ip, port);
     }
 
-    cout << "Scan terminé.\n";
+    for (auto& t : threads) {
+        t.join();
+    }
+
     return 0;
 }
